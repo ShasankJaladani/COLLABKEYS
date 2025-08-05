a6bf72cda536c1b7f932da0ca202c67336056009
ShasankJaladani <jaladanishasank@gmail.com>
2025-08-05 13:57:45 +0530

    first commit

---
+++ frontend/src/components/AdminDashboard/EventsSection.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "../../styles/updateEvent.css"
+const EventSection = () => {
+  const [events, setEvents] = useState([]);
+  const [newEvent, setNewEvent] = useState({ title: '', description: '', link: '' });
+
+  const fetchEvents = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_BACKEND_BASE_URL}/api/events`);
+      setEvents(response.data);
+    } catch (error) {
+      console.error('Error fetching events:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  const handleAddEvent = async () => {
+    try {
+      await axios.post(`${REACT_APP_BACKEND_BASE_URL/api/events}`, newEvent);
+      fetchEvents();
+      setNewEvent({ title: '', description: '', link: '' });
+    } catch (error) {
+      console.error('Error adding event:', error);
+    }
+  };
+
+  const handleDeleteEvent = async (id) => {
+    try {
+      await axios.delete(`${process.env.REACT_APP_BACKEND_BASE_URL}/api/events/${id}`);
+      fetchEvents();
+    } catch (error) {
+      console.error('Error deleting event:', error);
+    }
+  };
+
+  return (
+    <div className='gLayout'>
+      <div className='update'>
+      <h2>Manage Events</h2>
+        <input
+          type="text"
+          placeholder="Title"
+          value={newEvent.title}
+          onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+        />
+        <textarea
+          placeholder="Description"
+          value={newEvent.description}
+          onChange={(e) => setNewEvent({ ...newEvent, description: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Link"
+          value={newEvent.link}
+          onChange={(e) => setNewEvent({ ...newEvent, link: e.target.value })}
+        />
+        <button onClick={handleAddEvent}>Add Event</button>
+      </div>
+      <div className='event-detail'>
+      <h2 style={{textAlign:'center'}}>Active Events</h2>
+        {events.map((event) => (
+          <div key={event._id} className='event-card'>
+            <h3>{event.title}</h3>
+            <button onClick={() => handleDeleteEvent(event._id)}>Delete</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EventSection;
